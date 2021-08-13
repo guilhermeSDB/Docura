0c73c4f84b53b385b840f56dc16de1534f1a2f85
Guilherme Barros <Guilhermescarmagnani@outlook.com>
2021-08-12 22:52:18 -0300

    nova pagina quem somos

--- Pages/quem-somos.php
+++ Pages/quem-somos.php
@@ -1,12 +1,35 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+    <section>
+        <div class="quem-somos">
+            <h2>Quem Somos</h2>
+            <div class="quem-somos_section">
+                <div class="quem-somos_card">
+                    <div class="card_quem-somos">
+                        <div class="img_quem-somos">
+                            <img class="img_card" style="background-image: url('https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80');" alt="">
+                        </div>
+                        <hr class="divider">
+                        <div class="description_quem-somos">
+                            <h1>Anna Julia</h1>
+                            <p>Confeiteira Senior</p>
+                        </div>
+                    </div>                    
+                </div>      
+                <div class="quem-somos_card">
+                    <div class="card_quem-somos">
+                        <div class="img_quem-somos">
+                            <img class="img_card" style="background-image: url('https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80');" alt="">
+                        </div>
+                        <hr class="divider">
+                        <div class="description_quem-somos">
+                            <h1>Guilherme Barros</h1>
+                            <p>Programador</p>
+                        </div>
+                    </div>
+                    
+                </div>       
+            </div>
+        </div>
+    </section>
+
     
-</body>
-</html>
+
--- index.php
+++ index.php
@@ -18,6 +18,7 @@
     <link rel="stylesheet" href="css/global.css">  
     <link rel="stylesheet" href="css/home.css">   
     <link rel="stylesheet" href="css/todos-produtos.css"> 
+    <link rel="stylesheet" href="css/quem-somos.css"> 
 
     <!-- Carousel CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css" integrity="sha512-sMXtMNL1zRzolHYKEujM2AqCLUR9F2C4/05cdbxjjLSRvMQIciEPCQZo++nk7go3BtSuK9kfa/s+a4f4i5pLkw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -109,8 +110,8 @@
             });
             $("#Produtos").click(function(){
                 $("#main").load("Pages/todos-produtos.php");
-            });
-            $("#Quem-Somos").click(function(){
+            });                        
+            $("#Quem-somos").click(function(){
                 $("#main").load("Pages/quem-somos.php");
             });
         });
